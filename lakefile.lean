92a0b49300c586dd63d119c98a07fccfda508ec3
Johannes <johannes.tantow@gmail.com>
2023-10-02 23:52:10 +0200

    Start

---
+++ lakefile.lean
@@ -0,0 +1,12 @@
+import Lake
+open Lake DSL
+
+package «certifyingDatalog» where
+  -- add any package configuration options here
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+@[default_target]
+lean_lib «CertifyingDatalog» where
+  -- add any library configuration options here
