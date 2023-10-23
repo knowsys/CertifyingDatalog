a5ef330dc7fba5a582d3551f74157f92fab605b8
jt0202 <johannes.tantow@gmail.com>
2023-10-23 17:50:03 +0200

    Grounding model

--- CertifyingDatalog/Datalog.lean
+++ CertifyingDatalog/Datalog.lean
@@ -7,13 +7,12 @@ structure signature :=
   (relationSymbols: Type)
   (relationArity: relationSymbols → ℕ)
 
-
 section basic
 variable (τ: signature)
 
 inductive term : Type
 | constant : τ.constants → term
-| variable : τ.vars → term
+| variableDL : τ.vars → term
 
 structure atom :=
   (symbol: τ.relationSymbols)
@@ -24,7 +23,7 @@ structure rule :=
   (head: atom τ)
   (body: List (atom τ))
 
-def Program: Type := List (rule τ)
+def Program: Type := Finset (rule τ)
 
 end basic
 -- grounding
@@ -34,7 +33,7 @@ variable (τ: signature) [DecidableEq τ.vars]
 
 def termVariables: term τ → Finset τ.vars
 | (term.constant _) => ∅
-| (term.variable v) => {v}
+| (term.variableDL v) => {v}
 
 def collectResultsToFinset {A: Type} (f: A → Finset τ.vars): List A → Finset τ.vars
 | [] => ∅ 
@@ -52,7 +51,7 @@ def ruleVariables (r: rule τ): Finset τ.vars := (atomVariables τ r.head) ∪ 
 
 structure groundRule :=
   (rule: rule τ)
-  (grounded: ruleVariables τ r = ∅)
+  (grounded: ruleVariables τ rule = ∅)
 
 def groundRule.toRule (gr: groundRule τ) := gr.rule
 
@@ -61,7 +60,21 @@ def grounding:= τ.vars → τ.constants
 def applyGroundingTerm (g: grounding τ) (t: term τ): term τ :=
   match t with
   | term.constant c => term.constant c
-  | term.variable v => term.constant (g v)
+  | term.variableDL v => term.constant (g v)
+
+lemma applyGroundingTermRemovesVariables (g: grounding τ) (t: term τ): termVariables τ (applyGroundingTerm τ g t) = ∅ :=
+by
+  cases t with
+  | constant c =>
+    unfold applyGroundingTerm
+    unfold termVariables
+    simp
+  | variableDL v =>
+    unfold applyGroundingTerm
+    unfold termVariables
+    simp
+    
+
 
 lemma applyGroundingTermPreservesLength (g:grounding τ) (a: atom τ): (List.map (applyGroundingTerm τ g) a.atom_terms ).length = τ.relationArity a.symbol := 
 by
@@ -70,20 +83,50 @@ by
   rw [ term_length]
 
 
-def applyGroundingAtom (a: atom τ) (g: grounding τ): atom τ := {symbol:= a.symbol, atom_terms:= List.map (applyGroundingTerm τ g) a.atom_terms, term_length := applyGroundingTermPreservesLength τ g a}
+def applyGroundingAtom (g: grounding τ) (a: atom τ): atom τ := {symbol:= a.symbol, atom_terms:= List.map (applyGroundingTerm τ g) a.atom_terms, term_length := applyGroundingTermPreservesLength τ g a}
 
-def substitution := τ.vars → Option τ.constants
+lemma groundingRemovesAtomVariables (a: atom τ) (g: grounding τ): atomVariables τ (applyGroundingAtom τ g a) = ∅ :=
+by
+  unfold applyGroundingAtom
+  unfold atomVariables
+  simp
+  induction a.atom_terms with
+  | nil => 
+    unfold collectResultsToFinset
+    simp
+  | cons hd tl ih =>
+    unfold collectResultsToFinset
+    simp
+    rw [ih, Finset.union_empty, applyGroundingTermRemovesVariables]
+
+def atomGrounding (a: atom τ) (g: grounding τ): groundAtom τ := {atom := applyGroundingAtom τ g a, grounded := groundingRemovesAtomVariables τ a g}
+
+def applyGroundingRule (r: rule τ) (g: grounding τ): rule τ := {head := applyGroundingAtom τ g r.head, body := List.map (applyGroundingAtom τ g) r.body }
+
+lemma groundingRemovesRuleVariables (r: rule τ) (g: grounding τ): ruleVariables τ (applyGroundingRule τ r g) = ∅ := by
+  unfold applyGroundingRule
+  unfold ruleVariables
+  simp
+  rw [groundingRemovesAtomVariables, Finset.empty_union]
+  induction r.body with
+  | nil =>
+    unfold collectResultsToFinset
+    simp
+  | cons hd tl ih =>
+    unfold collectResultsToFinset
+    simp
+    rw [ih, groundingRemovesAtomVariables, Finset.empty_union]
+
+def ruleGrounding (r: rule τ) (g:grounding τ): groundRule τ := {rule := applyGroundingRule τ r g, grounded := groundingRemovesRuleVariables τ r g}
+
+def groundProgram (p: Program τ): Finset (groundRule τ) := sorry
 
 
 end grounding
 section semantics
 variable (τ:signature) [DecidableEq τ.vars]
 
-def interpretation := Set (groundAtom τ)
-
 class database:=
   (contains: groundAtom τ → Prop)
-
-
 
 end semantics
