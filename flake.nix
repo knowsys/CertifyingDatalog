8d4f6228a086ad923d369e568ffeeb25a20b418e
monsterkrampe <monsterkrampe@users.noreply.github.com>
2024-09-05 11:26:54 +0200

    Add flake for dependencies

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Ruleset Classifier";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = [
+              pkgs.elan
+              pkgs.python3 
+              pkgs.python311Packages.rfc3987
+              pkgs.ruby
+            ];
+          };
+        }
+      );
+}
