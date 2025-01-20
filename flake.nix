23fde8e9b99a973a3cfbcf257d56c4aeca7daaa9
monsterkrampe <monsterkrampe@users.noreply.github.com>
2025-01-20 14:28:55 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A certified checker for Datalog entailments, written in Lean";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
