6f7effc45e7f2696ab2e83ab9a3bdec50e6febbc
monsterkrampe <monsterkrampe@users.noreply.github.com>
2025-06-25 13:43:43 +0200

    Upgrade to Lean 4.20.1

--- lakefile.lean
+++ lakefile.lean
@@ -10,7 +10,7 @@ lean_lib «CertifyingDatalog» {
 }
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4"@"v4.20.0"
+  "https://github.com/leanprover-community/mathlib4"@"v4.20.1"
 
 @[default_target]
 lean_exe «certifyingDatalog» where
