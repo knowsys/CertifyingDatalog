fd53685d56340958a0ea0208c38b567949b14df1
monsterkrampe <monsterkrampe@users.noreply.github.com>
2026-06-11 08:02:20 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,22 +1,23 @@
 {
   description = "A certified checker for Datalog entailments, written in Lean";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "https://channels.nixos.org/nixos-26.05/nixexprs.tar.xz";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          devShells.default = pkgs.mkShell {
-            packages = [
-              pkgs.elan
-              pkgs.python3
-              pkgs.python311Packages.rfc3987
-              pkgs.ruby
-            ];
-          };
-        }
-      );
+  outputs = { self, nixpkgs }:
+    let
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"] (system: function nixpkgs.legacyPackages.${system});
+    in {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.elan
+            (pkgs.python3.withPackages (python-pkgs: [
+              python-pkgs.rfc3987
+            ]))
+            pkgs.ruby
+          ];
+        };
+      });
+    };
 }
