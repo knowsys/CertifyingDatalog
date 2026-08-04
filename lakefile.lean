8af84cd8e4f5ac90b625a70f63e9881c949ebc45
jt0202 <johannes.tantow@gmail.com>
2026-08-04 14:52:02 +0200

    Update

--- lakefile.lean
+++ lakefile.lean
@@ -10,7 +10,7 @@ lean_lib «CertifyingDatalog» {
 }
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4"@"v4.32.0"
+  "https://github.com/leanprover-community/mathlib4"@"v4.32.2"
 
 @[default_target]
 lean_exe «certifyingDatalog» where
